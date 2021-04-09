ef161407f35326ec1949520f24f8d1140683fda3
Jumscrafteur <hsansane@gmail.co>
2021-04-09 23:57:37 +0200

    ⚙️ v0.2.2: - Code refactoring : split in components - Add Manifest an service worker

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,10 @@
 <script>
+	import Header from "./components/Header.svelte";
+	import Option from "./components/Option.svelte";
+	import SizeSelect from "./components/SizeSelect.svelte";
+	import ItemContainer from "./components/ItemContainer.svelte";
+	import Footer from "./components/Footer.svelte";
+
 	let optTaille = "M";
 	let optDoublePoulet = false;
 	let optTenders = false;
@@ -7,49 +13,6 @@
 	let nbSupplements;
 	let tacos = { viandes: [], sauces: [], supplements: [] };
 	let charQueue = [];
-
-	let url = {
-		"Poulet nature": "chicken",
-		"Poulet mariné": "chicken-marinated",
-		"Viande hachée": "chopped-meat",
-		Merguez: "sausage",
-		Nuggets: "nuggets",
-		"Cordon bleu": "cordon-blue",
-		Tenders: "tenders",
-		Falafels: "falafel",
-
-		Algerienne: "algerian",
-		Barbecue: "bbq",
-		Biggy: "biggy",
-		"Chili-thai": "thai-chili",
-		Curry: "curry",
-		Fuego: "fuego",
-		Ketchup: "ketchup",
-		Mayonnaise: "mayonnaise",
-		Samourai: "samourai",
-
-		Gouda: "gouda",
-		Oignons: "caramelized-onions",
-		Lardons: "chicken-bacon",
-		Pastrami: "pastrami",
-		Poivronnade: "paprika",
-		"Vache qui rit": "vache-qui-rit",
-		Boursin: "boursin",
-		Bacon: "turkey-bacon",
-		Raclette: "raclette",
-		Cheddar: "cheddar",
-		Chèvre: "goat-cheese",
-		Mozzarella: "mozzarella",
-		Champignons: "mushrooms",
-		Jalapeno: "jalapeno-cheese",
-
-		"Frit'OTacos": "fries-otacos",
-		"Barquette de Frites ": "fries",
-
-		"Coca-Cola": "coca-cola",
-		Fanta: "fanta",
-		Tropico: "tropico",
-	};
 
 	let tailles = {
 		M: 1,
@@ -151,7 +114,7 @@
 		}
 	};
 
-	function handleClick() {
+	function refresh() {
 		tacos.viandes = choisirViandes(optTaille);
 		tacos.sauces = choisirSauces();
 		tacos.supplements = choisirSupplements(optSupplements);
@@ -160,46 +123,18 @@
 
 <svelte:window on:keydown={handleKeydown} />
 
-<div class="header">
-	<img src="/images/logo_text.svg" alt="" height="52px" class="logo" />
-	<img src="/images/folder.svg" alt="" height="14px" class="icon disabled" />
-</div>
+<Header />
 <main>
 	<div class="optBar">
 		<div class="optItems">
-			<div class="optItem">
-				<label for="optSupplements">Suppléments</label><input
-					id="optSupplements"
-					type="number"
-					min="0"
-					max="14"
-					bind:value={optSupplements}
-				/>
-			</div>
-			<div class="optItem" class:selected={optDoublePoulet}>
-				<input
-					id="optDoublePoulet"
-					type="checkbox"
-					bind:checked={optDoublePoulet}
-				/>
-				<label for="optDoublePoulet">Double poulet </label>
-			</div>
-			<div class="optItem" class:selected={optTenders}>
-				<input
-					id="optTenders"
-					type="checkbox"
-					bind:checked={optTenders}
-				/>
-				<label for="optTenders">Tenders</label>
-			</div>
-			<div class="optItem" class:selected={optFalafels}>
-				<input
-					id="optFalafels"
-					type="checkbox"
-					bind:checked={optFalafels}
-				/>
-				<label for="optFalafels">Falafels</label>
-			</div>
+			<Option
+				checkbox={false}
+				label={"Suppléments"}
+				bind:value={optSupplements}
+			/>
+			<Option label={"Double poulet"} bind:value={optDoublePoulet} />
+			<Option label={"Tenders"} bind:value={optTenders} />
+			<Option label={"Falafels"} bind:value={optFalafels} />
 		</div>
 
 		<div class="icon">
@@ -214,85 +149,23 @@
 				alt="refresh arrow"
 				class="icon"
 				height="20px"
-				on:click={handleClick}
+				on:click={refresh}
 			/>
 		</div>
 	</div>
 	<h2>Tailles</h2>
 	<div class="sizeBar">
-		<div class="sizeItems">
-			<div class="sizeItem" class:selected={optTaille == "M"}>
-				<input type="radio" bind:group={optTaille} value={"M"} id="M" />
-				<label for="M">M</label>
-			</div>
-			<div class="sizeItem" class:selected={optTaille == "L"}>
-				<input type="radio" bind:group={optTaille} value={"L"} id="L" />
-				<label for="L">L</label>
-			</div>
-			<div class="sizeItem" class:selected={optTaille == "XL"}>
-				<input
-					type="radio"
-					bind:group={optTaille}
-					value={"XL"}
-					id="XL"
-				/>
-				<label for="XL">XL</label>
-			</div>
-		</div>
+		<SizeSelect sizes={["M", "L", "XL"]} bind:value={optTaille} />
 	</div>
 	<hr class="separator" />
-	<h2>
-		Viande{#if optTaille != "M"}s{/if}
-	</h2>
-	<div class="contentBar">
-		{#each tacos.viandes as viande}
-			<div class="contentItem">
-				<img
-					src="https://o-tacos.com/soundboard/images/{url[
-						viande
-					]}_thumb.png"
-					alt=""
-				/>
-				<p>{viande}</p>
-			</div>
-		{/each}
-	</div>
-	<h2>
-		Sauce{#if optTaille != "M"}s{/if}
-	</h2>
-	<div class="contentBar">
-		{#each tacos.sauces as sauce}
-			<div class="contentItem">
-				<img
-					src="https://o-tacos.com/soundboard/images/{url[
-						sauce
-					]}_thumb.png"
-					alt=""
-				/>
-				<p>{sauce}</p>
-			</div>
-		{/each}
-	</div>
+	<ItemContainer Items={tacos.viandes} label={"Viande"} />
+	<ItemContainer Items={tacos.sauces} label={"Sauce"} />
+
 	{#if optSupplements > 0}
-		<h2>
-			Supplement{#if optSupplements > 1}s{/if}
-		</h2>
-		<div class="contentBar">
-			{#each tacos.supplements as supplement}
-				<div class="contentItem">
-					<img
-						src="https://o-tacos.com/soundboard/images/{url[
-							supplement
-						]}_thumb.png"
-						alt=""
-					/>
-					<p>{supplement}</p>
-				</div>
-			{/each}
-		</div>
+		<ItemContainer Items={tacos.supplements} label={"Supplement"} />
 	{/if}
 </main>
-<footer>Made with ❤️ by Jums & Johnny</footer>
+<Footer />
 
 <style lang="scss">
 	@import "./styles/vars";
@@ -309,32 +182,11 @@
 	* {
 		font-family: $mainFont;
 	}
-	.header {
-		width: 100%;
-		height: 78px;
-
-		background-color: $primary;
-		box-shadow: 0px 2px 0px 0px rgba(29, 29, 27, 0.1);
-
-		& .logo {
-			position: relative;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-		}
-
-		& .icon {
-			position: relative;
-			float: right;
-			top: 50%;
-			right: 14px;
-			transform: translate(0, -50%);
-		}
-	}
 
 	main {
 		padding: 11.6px 11.6px 50px 11.6px;
-		z-index: 0;
+		margin: 0 auto;
+		width: clamp(100px, 600px, 100%);
 		min-height: 100%;
 	}
 
@@ -363,40 +215,6 @@
 				background: $primary;
 				border-radius: 999px;
 			}
-
-			& .optItem {
-				display: flex;
-				align-items: stretch;
-				margin: 0 9.7px 0 0;
-				border: 2px solid $primary;
-				border-radius: 6.2px;
-
-				& label {
-					margin: 3px 6px;
-
-					color: $primary;
-					font-size: 12px;
-					white-space: nowrap;
-				}
-
-				& input[type="number"] {
-					// height: 27px;
-					width: 21px;
-
-					background-color: $primary;
-					border: none;
-					outline: none;
-
-					color: $white;
-					text-align: center;
-				}
-				& input[type="checkbox"] {
-					display: none;
-				}
-				&.selected {
-					background-color: $soft;
-				}
-			}
 		}
 		& .icon {
 			margin: 0 0 0 auto;
@@ -418,77 +236,6 @@
 		& .sizeItems {
 			margin-top: 15.5px;
 			display: flex;
-
-			& .sizeItem {
-				margin: 0 10px 0 0;
-				height: 29px;
-				width: 29px;
-
-				display: flex;
-
-				border: 2px solid $primary;
-				border-radius: 4.5px;
-
-				& input[type="radio"] {
-					display: none;
-				}
-
-				& label {
-					margin: auto;
-				}
-
-				&.selected {
-					background-color: $soft;
-				}
-			}
-		}
-	}
-
-	.contentBar {
-		display: flex;
-
-		overflow-x: auto;
-		overflow-y: none;
-
-		padding: 0 0 10px 0;
-
-		&::-webkit-scrollbar {
-			width: 5px;
-			height: 5px;
-		}
-
-		&::-webkit-scrollbar-track {
-			box-shadow: inset 0 0 5px $soft;
-			border-radius: 10px;
-		}
-
-		&::-webkit-scrollbar-thumb {
-			background: $primary;
-			border-radius: 999px;
-		}
-
-		& .contentItem {
-			margin: 0 20px 0 0;
-			width: 100px;
-			height: 100px;
-
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			flex-shrink: 0;
-
-			border: 2px solid $primary;
-			border-radius: 7px;
-
-			color: $primary;
-			text-align: center;
-			font-size: 14px;
-
-			& img {
-				margin-bottom: -6px;
-				width: 80px;
-				height: 80px;
-			}
 		}
 	}
 
@@ -497,23 +244,6 @@
 		width: 26px;
 		border: 2px solid $primary;
 		border-radius: 9999px;
-	}
-
-	footer {
-		position: absolute;
-		bottom: 0;
-		margin: 10px 0 0 0;
-		height: 38px;
-		width: 100%;
-
-		display: flex;
-		align-items: center;
-		justify-content: center;
-
-		color: $white;
-		text-align: center;
-
-		background-color: $primary;
 	}
 
 	h2 {
